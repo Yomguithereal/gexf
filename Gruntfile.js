628358169ce67048495d8d8503855086d5650458
Yomguithereal <guillaumeplique@gmail.com>
2014-01-08 15:28:09 +0100

    Adding jshint to grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,14 @@
 module.exports = function(grunt) {
+
+  var jsfiles = ['src/parser.js'];
+
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     closureLint: {
       app:{
         closureLinterPath: '/usr/local/bin',
-        src: ['src/parser.js'],
+        src: jsfiles,
         command: 'gjslint',
         options: {
           stdout: true,
@@ -29,16 +32,25 @@ module.exports = function(grunt) {
       },
       prod: {
         files: {
-          'build/gexf-parser.min.js': ['src/parser.js']
+          'build/gexf-parser.min.js': jsfiles
         }
       }
-    }
+    },
+    jshint: {
+      all: jsfiles,
+      options: {
+        '-W055': true,
+        '-W040': true,
+        '-W064': true
+      }
+    },
   });
 
   grunt.loadNpmTasks('grunt-closure-linter');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // By default, will check lint, test and minify:
-  grunt.registerTask('default', ['closureLint', 'qunit', 'uglify']);
+  grunt.registerTask('default', ['closureLint', 'jshint', 'qunit', 'uglify']);
 };
